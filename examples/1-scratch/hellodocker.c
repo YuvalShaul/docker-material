23d4685f15309eeb60d55ecfbfb0bbf1a5993fe1
Yuval Shaul <yuval.shaul@gmail.com>
2022-10-12 09:16:50 -0400

    updating some c files

--- examples/1-scratch/hellodocker.c
+++ examples/1-scratch/hellodocker.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 void main()
 {
-   printf("Hello Docker !!!\n");
+   int i;
+ 
+   for (i=0; i<1000 ; i++)
+   {
+      printf("Hello Docker !!!\n");
+      sleep(2);
+   }
 }
 
